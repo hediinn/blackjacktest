c49b3d07587287c1b55638483639377d892bf562
hediinn <hediinn@gmail.com>
2025-08-04 15:56:27 +0100

    stuff

--- main.c
+++ main.c
@@ -32,7 +32,14 @@ typedef struct card{
     char *suit;
 } card ;
 
-typedef card cards[52] ;
+typedef card deck[52] ;
+
+typedef struct decks{
+    card cards[52];
+    size_t size;
+    
+} decks ;
+
 
 void swap(card *restrict c1, card *restrict c2) {
         card s;
@@ -40,22 +47,28 @@ void swap(card *restrict c1, card *restrict c2) {
         *c1 = *c2;
         *c2 = s;
 }
+card draw(deck d) {
+    card c = d[0];
+    for(int i = 0;i<51;i++){
+        d[i] = d[i+1];
+    }
+    return c;
+}
 
-
-void cutDeck(cards ccs){
+void cutDeck(deck ccs){
     for(int i = 0;i<26;i++){
         //printf("card1: %s %s | card2: %s %s \n",ccs[i].name, ccs[i].suit,ccs[i+26].name, ccs[i+26].suit);
         swap(&ccs[i], &ccs[i+26]);
     }
 }
-void shuffle(cards ccs){
+void shuffle(deck ccs){
     for(int i = 0;i<52;i++){
         int ran = rand()%52;
         swap(&ccs[ran], &ccs[i]);
     }
 }
-void Diff_shuffle(cards ccs){
-    cards cd;
+void Diff_shuffle(deck ccs){
+    deck cd;
     for(int i = 0;i<26;i++){
         int c1 = i;
         int c2 = i+26;
@@ -67,19 +80,24 @@ void Diff_shuffle(cards ccs){
         swap(&ccs[i], &cd[i]);
     }
 }
-void printDeck(cards ccs){
+void printDeck(deck ccs){
     card s;
     for (int i = 0; i<52; i++) {
         s = ccs[i]; 
-        printf("card name: %s of %s | val: %d \n", s.name, s.suit, s.num);
+        if(i % 2 == 0){
+            printf("card name: %-5s of %-7s | val: %-2d ##", s.name, s.suit, s.num);
+        }else{
+            printf(" card name: %-5s of %-7s | val: %-2d \n", s.name, s.suit, s.num);
+        }
     }
+    printf("--------------------------------------------------------------\n");
 
 }
 
 int main() {
 
     srand(time(NULL));
-    cards cs;
+    deck cs;
     card c;
     int d = 0;
     for(int j = 0; j<4;j++){
@@ -97,18 +115,16 @@ int main() {
         }
     }
     card s;
-    for (int i = 0; i<3; i++) {
-        s = cs[i]; 
-        printf("card name: %s of %s | val: %d \n", s.name, s.suit, s.num);
-    } 
-    //shuffle(cs);
-    //cutDeck(cs);
     //shuffle(cs);
     Diff_shuffle(cs);
-    for (int i = 0; i<4; i++) {
-        s = cs[i]; 
-        printf("card name: %s of %s | val: %d \n", s.name, s.suit, s.num);
-    }   
+    Diff_shuffle(cs);
+    cutDeck(cs);
+    shuffle(cs);
+    printDeck(cs);
+    //for (int i = 0; i<4; i++) {
+    s = draw(cs); 
+    printf("card name: %s of %s | val: %d \n", s.name, s.suit, s.num);
+    //}   
     
     return 0;
 }
